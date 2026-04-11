30337ebe73f635b0351aaa27558f147512613240
pbenn28 <pbenn@stanford.edu>
2026-04-11 22:44:00 +0800

    Added requirements.txt

--- app/static/python/sunset_check.py
+++ app/static/python/sunset_check.py
@@ -6,9 +6,9 @@ def get_ip_lat_log(ip_address=""):
     try:
         response = requests.get(f"http://ip-api.com/json/{ip_address}?fields=lat,lon")
         response.raise_for_status()
-        data = response.json()  # was json.loads(response)
+        data = response.json()
         print(data)
         return data["lat"], data["lon"]
-    except:
-        print("Error getting IP")
+    except Exception as e:
+        print(f"Error getting IP: {e}")
         return (37.7749, -122.4194)
